cb90a2887b09b90879a8f7b49f485e5a019f10d6
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-02 10:22:36 +0300

    Update Point.cs

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,6 +12,17 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
+
+        public Point()
+        {
+            
+        }
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
 
         public void Draw()
         {
