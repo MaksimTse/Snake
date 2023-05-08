0a47bef4450ba087468447ff0bae768b8511116a
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-08 10:11:29 +0300

    Update Point.cs
    UL 8-10

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -18,11 +18,41 @@ namespace Snake
             y = _y;
             sym = _sym;
         }
+        public Point(Point p) 
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x+= offset;
+            }
+            else if(direction == Direction.LEFT)
+            {
+                x-= offset;
+            }
+            else if(direction == Direction.UP)
+            {
+                y+= offset;
+            }
+            else if(direction == Direction.DOWN)
+            {
+                y-= offset;
+            }
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
         }
 
     }
