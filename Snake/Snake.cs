8a432049ad8b57282b8adb4144dc9886d5832b4c
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-08 10:43:11 +0300

    Update Snake.cs
    Movement

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,6 +36,17 @@ namespace Snake
             Point nextPoint = new Point(head);
             nextPoint.Move(1 , direction);
             return nextPoint;
+        }
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
         }
     }
 }
