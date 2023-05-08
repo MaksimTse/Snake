ef2337d0318fa94c7b84623dae7333ba613e2a40
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-08 11:25:33 +0300

    Update Program.cs
    food

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -24,8 +24,21 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '¤');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while(true)
             {
+                if(snake.Eat( food ))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
