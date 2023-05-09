0c32849dea676a606dbe9136c2beeb75615fd989
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-09 11:40:28 +0300

    Add files via upload

---
+++ Snake/Music.cs
@@ -0,0 +1,95 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Snake
+{
+    public class Music
+    {
+        public async Task Background_music(String Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audiofileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent { DesiredLatency = 200, Volume = 50})
+                {
+                    waveOutDevice.Init(audiofileReader);
+                    waveOutDevice.Play();
+                    while(waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+        public async Task Music_Empty(String Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audiofileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent())
+                {
+                    waveOutDevice.Init(audiofileReader);
+                    waveOutDevice.Play();
+                    while (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(50);
+                    }
+                }
+            });
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Snake
+{
+    public class Music
+    {
+        public async Task Background_music(String Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audiofileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent { DesiredLatency = 200, Volume = 50})
+                {
+                    waveOutDevice.Init(audiofileReader);
+                    waveOutDevice.Play();
+                    while(waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+        public async Task Music_Empty(String Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audiofileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent())
+                {
+                    waveOutDevice.Init(audiofileReader);
+                    waveOutDevice.Play();
+                    while (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(50);
+                    }
+                }
+            });
+        }
+    }
+}
+>>>>>>> a2350a82402f8484b402aa67fc936b7526d73dd9
