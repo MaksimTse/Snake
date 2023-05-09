6c6ddf165f98983dbe4c80cf8c5edf7e2a7b5ec1
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-09 11:37:02 +0300

    Update Program.cs

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,70 +6,75 @@ using System.Threading;
 
 namespace Snake
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			Console.SetWindowSize( 80, 25 );
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
 
-			Walls walls = new Walls( 80, 25 );
-			walls.Draw();
-			
-			Point p = new Point( 4, 5, '*' );
-			Snake snake = new Snake( p, 4, Direction.RIGHT );
-			snake.Draw();
+            Music game = new Music();
+            ConsoleKeyInfo nupp = new ConsoleKeyInfo();
+            _ = game.Background_music("../../../filename.mp3");
 
-			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
-			Point food = foodCreator.CreateFood();
-			food.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-			while (true)
-			{
-				if ( walls.IsHit(snake) || snake.IsHitTail() )
-				{
-					break;
-				}
-				if(snake.Eat( food ) )
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-				{
-					snake.Move();
-				}
+            Point p = new Point(4, 5, '●');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-				Thread.Sleep( 100 );
-				if ( Console.KeyAvailable )
-				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey( key.Key );
-				}
-			}
-			WriteGameOver();
-			Console.ReadLine();
-      }
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    _ = game.Background_music("../../../filename.mp3");
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+            WriteGameOver();
+            Console.ReadLine();
+        }
 
 
-		static void WriteGameOver()
-		{
-			int xOffset = 25;
-			int yOffset = 8;
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.SetCursorPosition( xOffset, yOffset++ );
-			WriteText( "============================", xOffset, yOffset++ );
-			WriteText( "И Г Р А    О К О Н Ч Е Н А?", xOffset + 1, yOffset++ );
-			yOffset++;
-			WriteText( "Автор: Кто то", xOffset + 2, yOffset++ );
-			WriteText( "Специально для TTHK", xOffset + 1, yOffset++ );
-			WriteText( "============================", xOffset, yOffset++ );
-		}
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("G A M E   O V E R?", xOffset + 5, yOffset++);
+            yOffset++;
+            WriteText("Autor: Someone", xOffset + 6, yOffset++);
+            WriteText("Special for TTHK", xOffset + 5, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
 
-		static void WriteText( String text, int xOffset, int yOffset )
-		{
-			Console.SetCursorPosition( xOffset, yOffset );
-			Console.WriteLine( text );
-		}
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
 
-	}
+    }
 }
