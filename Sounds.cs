29b885fa68bd5d7823b4eed6f01b45ea7b089431
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-05-22 14:25:40 +0300

    Add files via upload

---
+++ Sounds.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.ComponentModel;
+using System.Numerics;
+using NAudio;
+using NAudio.Wave;
+
+namespace Snake
+{
+    public class Sounds
+    {
+        private IWavePlayer waveOutDevice;
+        private AudioFileReader audioFileReader;
+        private string pathToMedia;
+        private Sounds sounds;
+        public Sounds(string pathToResources)
+        {
+            pathToMedia = pathToResources;
+            waveOutDevice = new WaveOutEvent();
+            audioFileReader = new AudioFileReader(pathToMedia + "/Background_Music.mp3");
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+        }
+
+        public void PlayBack()
+        {
+            waveOutDevice.Stop();
+            audioFileReader = new AudioFileReader(pathToMedia + "/Background_Music.mp3");
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+        }
+
+        public void PlayEat()
+        {
+            waveOutDevice.Stop();
+            audioFileReader = new AudioFileReader(pathToMedia + "/Snack.mp3");
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+        }
+
+        public void PlayGameOver()
+        {
+            waveOutDevice.Stop();
+            audioFileReader = new AudioFileReader(pathToMedia + "/GameOver.mp3");
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+        }
+
+        public void Stop()
+        {
+            waveOutDevice.Stop();
+        }
+
+        public void Dispose()
+        {
+            waveOutDevice.Dispose();
+            audioFileReader.Dispose();
+        }
+    }
+}
